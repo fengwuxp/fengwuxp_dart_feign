e5ff4ba72cdaa88d611358304f6f5e8416f35da4
风 <1109695647@qq.com>
2020-04-27 18:14:50 +0800

    Signed-off-by: 风 <1109695647@qq.com>

---
+++ test/http/converter/json_object_http_message_converter.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:built_value/serializer.dart';
+import 'package:fengwuxp_dart_basic/index.dart';
+import 'package:fengwuxp_dart_openfeign/src/http/converter/http_message_converter.dart';
+import 'package:fengwuxp_dart_openfeign/src/http/http_input_message.dart';
+import 'package:fengwuxp_dart_openfeign/src/http/http_output_message.dart';
+
+import '../../built/serializers.dart';
+
+class JsonObjectHttpMessageConverter implements HttpMessageConverter<JsonSerializableObject> {
+  bool canRead(ContentType mediaType, {Serializer serializer}) {
+    return true;
+  }
+
+  bool canWrite(ContentType mediaType, {Serializer serializer}) {
+    return true;
+  }
+
+  List<ContentType> getSupportedMediaTypes() {
+    return [];
+  }
+
+  Future<E> read<E>(HttpInputMessage inputMessage, {Serializer<E> serializer}) {
+    var body = inputMessage.stream;
+
+    return body.bytesToString().then((data) {
+      var builtJsonSerializers = BuiltJsonSerializers(serializers);
+      var deserializeWith = builtJsonSerializers.parseObject(data, serializer);
+      return deserializeWith;
+    });
+  }
+
+  write(JsonSerializableObject data, ContentType mediaType, HttpOutputMessage outputMessage) {
+    outputMessage.body.add(data.toJson().codeUnits);
+  }
+}
